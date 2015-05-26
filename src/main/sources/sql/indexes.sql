a585c9ad9f6bd14b6b2f8e75d1290ab1f577cc21
thergbway <thergbworld@gmail.com>
2015-05-26 05:52:00 +0300

    Added sql queries for indexes creation

---
+++ src/main/sources/sql/indexes.sql
@@ -0,0 +1,6 @@
+create index role_id_index on role(id);
+create index user_total_id_index on user_total(id);
+create index log_creation_time_index on log(creation_time);
+create index meeting_id_index on meeting(id);
+create index manager_id_index on manager(id);
+create index participant_id_index on participant(id);
