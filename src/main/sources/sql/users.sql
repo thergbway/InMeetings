db48ebf626512623721813827baa8c111be84ea3
thergbway <thergbworld@gmail.com>
2015-05-26 05:49:02 +0300

    Added sql queries for new user creation

---
+++ src/main/sources/sql/users.sql
@@ -0,0 +1,23 @@
+create user proj_user with password '123';
+
+grant select on manager to proj_user;
+grant select on meeting to proj_user;
+grant select on participant to proj_user;
+grant select on role to proj_user;
+grant select on user_total to proj_user;
+
+grant update on manager to proj_user;
+grant update on meeting to proj_user;
+grant update on participant to proj_user;
+grant update on user_total to proj_user;
+
+grant insert on manager to proj_user;
+grant insert on meeting to proj_user;
+grant insert on participant to proj_user;
+grant insert on user_total to proj_user;
+
+grant usage, select on sequence manager_id_seq to proj_user;
+grant usage, select on sequence meeting_id_seq to proj_user;
+grant usage, select on sequence participant_id_seq to proj_user;
+grant usage, select on sequence role_id_seq to proj_user;
+grant usage, select on sequence user_total_id_seq to proj_user;
